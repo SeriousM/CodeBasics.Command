296d91986f8224fca45e3578266257ab2e4ae98a
julien.vandenbussche@axa.fr <julien.vandenbussche@axa.fr>
2018-05-16 16:56:05 +0200

    feat(add fix for guid type)

--- Command/Extensions/TypeExtensions.cs
+++ Command/Extensions/TypeExtensions.cs
@@ -1,13 +1,4 @@
-﻿// //  -----------------------------------------------------------------------
-// //  <copyright file="TypeExtensions.cs" company="AXA France Service">
-// //      Copyright (c) AXA France Service. All rights reserved.
-// //  </copyright>
-// //  <actor>S614599 (VANDENBUSSCHE Julien)</actor>
-// //  <created>30/11/2016 14:52</created>
-// //  <modified>16/01/2017 15:27</modified>
-// //  -----------------------------------------------------------------------
-
-namespace Command.Extensions
+﻿namespace Command.Extensions
 {
     using System;
     using System.Reflection;
@@ -17,7 +8,7 @@ namespace Command.Extensions
         public static bool IsPrimitive(this Type that)
         {
             return that.GetTypeInfo().IsPrimitive || that == typeof(string) || that == typeof(decimal)
-                   || that == typeof(DateTime);
+                   || that == typeof(DateTime) || that == typeof(Guid);
         }
     }
 }
